92f99287c30a29f7f620237bb0e846aa003757a8
NguyenAnhDuc1997 <soitrangtn123@gmail.com>
2019-09-16 22:06:13 +0700

    Commit source code

---
+++ CourseOnline/src/main/java/com/example/CourceOnline/CourseOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.example.CourceOnline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseOnlineApplication.class, args);
+	}
+
+}
