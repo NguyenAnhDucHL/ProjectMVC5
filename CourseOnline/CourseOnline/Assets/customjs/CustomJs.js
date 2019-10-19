db8f7f7d9bab4cf03358240c004d72b7c75cd96c
duongharzad <55396419+duongharzad@users.noreply.github.com>
2019-10-19 21:26:45 +0700

    commit soủce code c#

---
+++ CourseOnline/CourseOnline/Assets/customjs/CustomJs.js
@@ -0,0 +1,8 @@
++function ($) {
+    $("#example1").DataTable();
+}(jquery);
++function ($) {
+    // Replace the <textarea id="editor1"> with a CKEditor
+    // instance, using default configuration.
+    CKEDITOR.replace('editor1');
+}(jquery);
