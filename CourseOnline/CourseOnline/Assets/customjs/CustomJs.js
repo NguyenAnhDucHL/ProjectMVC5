6b1c5ee113b5e2d42af7e5fa646ea3688327af44
Nhatnqse04924 <nhatnqse04924@fpt.edu.vn>
2019-10-20 19:29:48 +0700

    nhat

--- CourseOnline/CourseOnline/Assets/customjs/CustomJs.js
+++
@@ -1,8 +0,0 @@
-+function ($) {
-    $("#example1").DataTable();
-}(jquery);
-+function ($) {
-    // Replace the <textarea id="editor1"> with a CKEditor
-    // instance, using default configuration.
-    CKEDITOR.replace('editor1');
-}(jquery);
