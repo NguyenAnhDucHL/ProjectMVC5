55d386148275bd7597d4113ceeb055735aca89b6
NguyenAnhDuc <soitrangtn123@gmail.com>
2019-10-31 22:57:23 +0700

    Add more page

---
+++ CourseOnline/CourseOnline/Controllers/CourseController.cs
@@ -0,0 +1,21 @@
+﻿using CourseOnline.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CourseOnline.Controllers
+{
+    public class CourseController : Controller
+    {
+        private STUDYONLINEEntities db = new STUDYONLINEEntities();
+        // GET: Course
+        public ActionResult ListCourse()
+        {
+
+           //var lstSubject = 
+            return View("/Views/User/Course.cshtml");
+        }
+    }
+}
