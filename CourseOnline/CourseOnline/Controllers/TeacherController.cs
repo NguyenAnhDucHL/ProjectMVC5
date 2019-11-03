8df55459fe0caf7002c6b4d96ef44c176ea79d95
NguyenAnhDuc <soitrangtn123@gmail.com>
2019-11-04 02:17:23 +0700

    Add Teacher code

---
+++ CourseOnline/CourseOnline/Controllers/TeacherController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CourseOnline.Models;
+using PagedList;
+
+namespace CourseOnline.Controllers
+{
+    public class TeacherController : Controller
+    {
+        private STUDYONLINEEntities db = new STUDYONLINEEntities();
+        // GET: Teacher
+        public ActionResult listAllTeacher(int? page)
+        {
+            int pageSize = 6;
+            int pageNumber = (page ?? 1);
+            var lstTeacher = (from u in db.Users
+                              join ur in db.UserRoles.Where(ur => ur.role_id == 2) on u.user_id equals ur.user_id
+                              select new TeacherModel
+                              {
+                                  user_fullname = u.user_fullname,
+                                  user_group = u.user_group,
+                                  user_image = u.user_image,
+                                  user_description = u.user_description,
+                              }).OrderBy(n => n.user_fullname).ToPagedList(pageNumber, pageSize);
+            ViewBag.lstTeacher = lstTeacher;
+            return View("/Views/User/TeacherList.cshtml");
+        }
+    }
+}
