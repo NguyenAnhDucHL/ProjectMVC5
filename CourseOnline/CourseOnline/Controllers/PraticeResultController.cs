b8b7de137aa3a95f5b372d0aa9d7e67b5cdfb906
NguyenAnhDuc <soitrangtn123@gmail.com>
2019-11-09 18:10:09 +0700

    commit source code

--- CourseOnline/CourseOnline/Controllers/PraticeResultController.cs
+++ CourseOnline/CourseOnline/Controllers/PraticeResultController.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Linq.Dynamic;
+using CourseOnline.Global.Setting;
+using System.Data.SqlClient;
 
 namespace CourseOnline.Controllers
 {
@@ -17,12 +19,6 @@ namespace CourseOnline.Controllers
             {
                 var listSubject = db.Subjects.Select(s => s.subject_name).Distinct().ToList();
                 ViewBag.listSubject = listSubject;
-
-                var listDomains = db.Domains.Select(s => s.domain_name).Distinct().ToList();
-                ViewBag.listDomains = listDomains;
-
-                var listLessons = db.Lessons.Select(s => s.lesson_name).Distinct().ToList();
-                ViewBag.listLessons = listLessons;
 
             }
             return View("~/Views/CMS/PracticeResult/PracticeResultList.cshtml");
@@ -57,10 +53,57 @@ namespace CourseOnline.Controllers
                 practiceListModels = practiceListModels.OrderBy(sortColumnName + " " + sortDirection).ToList();
                 return Json(new { success = true, data = practiceListModels, draw = Request["draw"], recordsTotal = totalrows, recordsFiltered = totalrowsafterfiltering }, JsonRequestBehavior.AllowGet);
             }
-            
+
         }
 
 
-
+        public ActionResult FilterBySubjectName(string subjectName)
+        {
+            int start = Convert.ToInt32(Request["start"]);
+            int length = Convert.ToInt32(Request["length"]);
+            string searchValue = Request["search[value]"];
+            string sortColumnName = Request["columns[" + Request["order[0][column]"] + "][name]"];
+            string sortDirection = Request["order[0][dir]"];
+            using (STUDYONLINEEntities db = new STUDYONLINEEntities())
+            {
+                if (!subjectName.Equals(All.ALL_Subject))
+                {
+                    string sql = "select u.[user_fullname], u.[user_email], s.[subject_name], e.[exam_name], tr.[tested_at], gr.[grade]" +
+                             "from [User] u join [TestResult] tr " +
+                             "on u.[user_id] = tr.[user_id] " +
+                             "join [Grade] gr " +
+                             "on u.user_id = gr.user_id " +
+                             "join [Exam] e " +
+                             "on tr.exam_id = e.exam_id " +
+                             "join [Subject] s " +
+                             "on e.subject_id = s.subject_id " +
+                             "WHERE s.subject_name = @subject_name";
+                    List<PracticeResultModel> userListModels = db.Database.SqlQuery<PracticeResultModel>(sql, new SqlParameter("subject_name", subjectName)).ToList();
+                    int totalrows = userListModels.Count;
+                    int totalrowsafterfiltering = userListModels.Count;
+                    userListModels = userListModels.Skip(start).Take(length).ToList();
+                    userListModels = userListModels.OrderBy(sortColumnName + " " + sortDirection).ToList();
+                    return Json(new { success = true, data = userListModels, draw = Request["draw"], recordsTotal = totalrows, recordsFiltered = totalrowsafterfiltering }, JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    string sql = "select u.[user_fullname], u.[user_email], s.[subject_name], e.[exam_name], tr.[tested_at], gr.[grade]" +
+                             "from [User] u join [TestResult] tr " +
+                             "on u.[user_id] = tr.[user_id] " +
+                             "join [Grade] gr " +
+                             "on u.user_id = gr.user_id " +
+                             "join [Exam] e " +
+                             "on tr.exam_id = e.exam_id " +
+                             "join [Subject] s " +
+                             "on e.subject_id = s.subject_id";
+                    List<PracticeResultModel> userListModels = db.Database.SqlQuery<PracticeResultModel>(sql).ToList();
+                    int totalrows = userListModels.Count;
+                    int totalrowsafterfiltering = userListModels.Count;
+                    userListModels = userListModels.Skip(start).Take(length).ToList();
+                    userListModels = userListModels.OrderBy(sortColumnName + " " + sortDirection).ToList();
+                    return Json(new { success = true, data = userListModels, draw = Request["draw"], recordsTotal = totalrows, recordsFiltered = totalrowsafterfiltering }, JsonRequestBehavior.AllowGet);
+                }
+            }
+        }
     }
 }
