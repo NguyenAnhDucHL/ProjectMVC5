a154dd967dc02d57eadd679e0a89034a23be494e
NguyenAnhDuc <soitrangtn123@gmail.com>
2019-12-01 00:03:38 +0700

    commit source

--- CourseOnline/Controllers/NewsController.cs
+++ CourseOnline/Controllers/NewsController.cs
@@ -20,5 +20,23 @@ namespace CourseOnline.Controllers
             ViewBag.lstPost = lstPost;
             return View("/Views/User/Post.cshtml");
         }
+        public ActionResult ResourcesList(int? page)
+        {
+            int pageSize = 4;
+            int pageNumber = (page ?? 1);
+            var lstPost = db.Posts.OrderBy(n => n.post_id).Where(post => post.post_status == "Published" && post.post_type == "Resourse").ToPagedList(pageNumber, pageSize);
+            ViewBag.lstPost = lstPost;
+            ViewData["PostType"] = "Resource Post";
+            return View("/Views/User/Post.cshtml");
+        }
+        public ActionResult GuideList(int? page)
+        {
+            int pageSize = 4;
+            int pageNumber = (page ?? 1);
+            var lstPost = db.Posts.OrderBy(n => n.post_id).Where(post => post.post_status == "Published" && post.post_type == "Guide").ToPagedList(pageNumber, pageSize);
+            ViewBag.lstPost = lstPost;
+            ViewData["PostType"] = "Guide Post";
+            return View("/Views/User/Post.cshtml");
+        }
     }
 }
