c82f60087b69622fe4816bd0f24a67f55e9e8755
khac phat <khacphi97@gmail.com>
2019-12-12 10:59:21 -0800

    Commit Code

--- CourseOnline/Controllers/HomeController.cs
+++ CourseOnline/Controllers/HomeController.cs
@@ -111,7 +111,7 @@ namespace CourseOnline.Controllers
             }
             else
             {
-                return RedirectToAction("Home_User", "Home");
+                return RedirectToAction("YourCourse", "Home");
             }
         }
 
