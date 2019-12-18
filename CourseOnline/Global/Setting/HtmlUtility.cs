8be9aef1ecad40c4029fdbcbd4684681d606a487
NguyenAnhDuc <soitrangtn123@gmail.com>
2019-12-18 20:27:15 +0700

    fix layout

---
+++ CourseOnline/Global/Setting/HtmlUtility.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CourseOnline.Global.Setting
+{
+    public static class HtmlUtility
+    {
+          public static string IsActive(this UrlHelper html,
+                                  string control,
+                                  string action)
+          {
+            var routeAction = (string)html.RequestContext.RouteData.Values["controller"];
+            var routeControl = (string)html.RequestContext.RouteData.Values["action"];
+
+            // must match both
+            var returnActive = control == routeControl &&
+                               action == routeAction;
+
+            return returnActive ? "active" : "";
+        }
+    }
+}
